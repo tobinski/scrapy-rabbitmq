75817256ba0de5b684962ea19a0dc4edb7ac3f15
tmen <you@example.com>
2015-10-06 15:50:34 +0200

    Compatibility to pika 0.10

--- scrapy_rabbitmq/connection.py
+++ scrapy_rabbitmq/connection.py
@@ -15,7 +15,7 @@ def from_settings(settings):
     """ Factory method that returns an instance of channel
     
         :param str connection_type: This field can be `blocking`
-            `asyncore`, `libev`, `select`, `tornado`, or `twisted`
+            `libev`, `select`, `tornado`, or `twisted`
 
         See pika documentation for more details:
             TODO: put pika url regarding connection type
@@ -47,7 +47,6 @@ def from_settings(settings):
 
     connection = {
         'blocking': pika.BlockingConnection,
-        'asyncore': pika.AsyncoreConnection,
         'libev': pika.LibevConnection,
         'select': pika.SelectConnection,
         'tornado': pika.TornadoConnection,
